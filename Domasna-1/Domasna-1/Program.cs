e18b7d0b6d07fddf6f86ffd44149c3dd06f7fa0c
Vedra-Stojanovska <vedrastojanovska@gmail.com>
2022-03-07 23:32:30 +0100

    prva-domasna-basic-c#

---
+++ Domasna-1/Domasna-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Domasna_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("       *  ");
+           Console.WriteLine("       *  ");
+           Console.WriteLine("    *  *  *");
+           Console.WriteLine(" *     *     *");
+           Console.WriteLine("       *  ");
+           Console.WriteLine("       *  ");
+           Console.WriteLine("    *     *");
+           Console.WriteLine("  *         *");
+        }
+    }
+}
