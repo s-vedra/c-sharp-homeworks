26e34b3bd5a0f562cddd7f053482cd6786274e12
Vedra-Stojanovska <vedrastojanovska@gmail.com>
2022-03-10 21:02:13 +0100

    Domasna-2

---
+++ Domasna 2/Domasna 2-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domasna_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int credits = 102;
+            int smsCost = 5;
+            Console.WriteLine("You can send " + credits / smsCost + " SMS messages");
+        }
+    }
+}
---
+++ Domasna 2/Domasna 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Domasna_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstDouble = 2.5;
+            double secondDouble = 6.8;
+            double resOne = secondDouble / firstDouble;
+            Console.WriteLine(resOne);
+            int firstInt = 10;
+            int secondInt = 20;
+            int resTwo = secondInt / firstInt;
+            Console.WriteLine(resTwo);
+            Console.WriteLine(resTwo > resOne);
+            Console.WriteLine(resOne > resTwo);
+        }
+    }
+}
