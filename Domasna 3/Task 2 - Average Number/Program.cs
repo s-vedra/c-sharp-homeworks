4019aba8beb50e5d109614dd1f56966b498347cb
Vedra-Stojanovska <vedrastojanovska@gmail.com>
2022-03-16 00:45:34 +0100

    Update Program.cs

--- Domasna 3/Task 2 - Average Number/Program.cs
+++ Domasna 3/Task 2 - Average Number/Program.cs
@@ -6,24 +6,19 @@ namespace Task_2___Average_Number
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter first number");
-            string firstInput = Console.ReadLine();
-            Console.WriteLine("Enter second number");
-            string secondInput = Console.ReadLine();
-            Console.WriteLine("Enter third number");
-            string thirdInput = Console.ReadLine();
-            Console.WriteLine("Enter fourth number");
-            string fourthInput = Console.ReadLine();
-            if (!int.TryParse(firstInput, out int firstNum) || 
-                !int.TryParse(secondInput, out int secondNum)||
-                !int.TryParse(thirdInput, out int thirdNum) || 
-                !int.TryParse(fourthInput, out int fourthNum))
+
+            Console.WriteLine("Enter four numbers");
+
+            if (!decimal.TryParse(Console.ReadLine(), out decimal firstNum) || 
+                !decimal.TryParse(Console.ReadLine(), out decimal secondNum)||
+                !decimal.TryParse(Console.ReadLine(), out decimal thirdNum) || 
+                !decimal.TryParse(Console.ReadLine(), out decimal fourthNum))
             {
                 Console.WriteLine("Enter valid number");
                 return;
             }
             
-            Console.WriteLine("The average is " + ((decimal) firstNum + secondNum + thirdNum +  fourthNum) / 4);
+            Console.WriteLine("The average is " + (firstNum + secondNum + thirdNum +  fourthNum) / 4);
         }
     }
 }
