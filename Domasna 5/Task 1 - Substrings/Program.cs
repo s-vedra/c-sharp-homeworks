912f6c5da55ad50d55cf8951ff4da709536f774c
Vedra-Stojanovska <vedrastojanovska@gmail.com>
2022-03-18 19:51:58 +0100

    domasna-5

---
+++ Domasna 5/Task 1 - Substrings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_1___Substrings
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Substrings();
+        }
+        public static void Substrings()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a number");
+                string userInput = Console.ReadLine();
+                string one = "Hello from SEDC Codeacademy 2022";
+
+                if (!int.TryParse(userInput, out int number) || number > one.Length)
+                {
+                    Console.WriteLine(one);
+                    continue;
+                }
+               Console.WriteLine($"The first n characters from the string are {one[..number]} and the new length is {number}");
+               break;
+            }
+
+        }
+    }
+}
+
---
+++ Domasna 5/Task 2 - Age Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task_2___Age_Calculator
+{
+    internal class Program
+    {
+        public static void BirthDate()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter birth year, birth month and birth day in the respective order");
+                if (!int.TryParse(Console.ReadLine(), out int year) ||
+                !int.TryParse(Console.ReadLine(), out int month) ||
+                !int.TryParse(Console.ReadLine(), out int day))
+                {
+                    Console.WriteLine("Invalid number");
+                    continue;
+                } 
+                    Console.WriteLine($"Your age is {AgeCalculator(year, month, day)}");
+                    break;
+            }
+        }
+        public static int AgeCalculator(int year, int month, int day)
+        {
+            DateTime currentDate = DateTime.UtcNow;
+            DateTime date = new DateTime(year, month, day);
+            int age = currentDate.Year - date.Year;
+            if (currentDate.Month < date.Month || (currentDate.Month == date.Month && currentDate.Day < date.Day))
+            {
+                age--;
+            }
+            return age;
+        }
+        static void Main(string[] args)
+        {
+            BirthDate();
+            
+        }
+    }
+}
